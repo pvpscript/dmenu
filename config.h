0ea6b9313f5094037586a3245500c0f8793a9752
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-04-05 23:55:09 -0300

    Changed color scheme to black-blueish

--- config.h
+++ config.h
@@ -10,9 +10,12 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/* [SchemeNorm] = { "#bbbbbb", "#222222" }, */
+	/* [SchemeSel] = { "#eeeeee", "#005577" }, */
+	/* [SchemeOut] = { "#000000", "#00ffff" }, */
+	[SchemeNorm] = { "#dbdcd1", "#0c1220" },
+	[SchemeSel] = { "#dbdcd1", "#70A9A7" },
+	[SchemeOut] = { "#dbdcd1", "#BEC2B3" },	
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
