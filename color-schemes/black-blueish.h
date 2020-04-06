5daab050affa059ec6276850396d5aefd38516f1
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-04-05 23:52:51 -0300

    Color schemes generated by pywal

---
+++ color-schemes/black-blueish.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#dbdcd1", "#0c1220" },
+	[SchemeSel] = { "#dbdcd1", "#70A9A7" },
+	[SchemeOut] = { "#dbdcd1", "#BEC2B3" },
+};
---
+++ color-schemes/black-white.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#e6e6e7", "#0a0b0a" },
+	[SchemeSel] = { "#e6e6e7", "#8C8F92" },
+	[SchemeOut] = { "#e6e6e7", "#CDCDCF" },
+};
